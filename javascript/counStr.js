703b82bf433752c941a880add1db845de5692940
DanielCodex <danialups1997@gmail.com>
2022-07-25 10:48:45 +0200

    i don't remember what i did

--- javascript/counStr.js
+++ javascript/counStr.js
@@ -3,10 +3,10 @@
  * @param {string} str
  */
 function countStr(str) {
-  str = str.toLowerCase().replace(/\s+|\W/g, '');
+  str = str.toLowerCase().replace(/\s+|\W/g, "");
   let res = {};
   for (let i = 0; i < str.length; i++) {
-    let char = str[i]
+    let char = str[i];
     // if (!res[char]) {
     //   res[char] = 1;
     // } else {
@@ -20,3 +20,19 @@ function countStr(str) {
 let a = "danile   "; // d:1 a:1 n:1 i:1 e:1 l:1
 console.log(countStr(a));
 
+/**
+ *
+ * @param {string} char
+ * @returns
+ */
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0—9)
+    !(code > 64 && code < 91) && // upper alpha (A—Z)
+    !(code > 96 && code < 123) // lower alphaa (a-a)
+  ) {
+    return false;
+  }
+  return true;
+}
