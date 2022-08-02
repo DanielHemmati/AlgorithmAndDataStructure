3a2f1312ce41352d0c9ad2ce181508173219ebd2
DanielCodex <danialups1997@gmail.com>
2022-08-02 11:17:28 +0200

    it's good to know

--- cpp/recursion/taylor_with_horeners_rec.cpp
+++ cpp/recursion/taylor_with_horeners_rec.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+double tayloWithLoop(int x, int n) {
+  double s = 1;
+  double numerator = 1;
+  double denominator = 1;
+
+  for (int i = 1; i <= n; i++){
+    numerator *= x;
+    denominator *= i;
+    s += numerator / denominator;
+  }
+  return s;
+}
+
+// i don't like the student problem lol
+double taylorRec(int x, int n) {
+  static double s;
+  if (n == 0) {
+    return s;
+  }
+  s = 1 + x / n * s;
+  return taylorRec(x, n - 1);
+}
 
 int main() {
+  cout << tayloWithLoop(1, 10) << endl;
   return 0;
 }
