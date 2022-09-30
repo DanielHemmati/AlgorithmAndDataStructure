1c26d74162d39e6d63ebc489be9d711ac8daafba
DanielCodex <danialups1997@gmail.com>
2022-09-30 11:58:32 +0200

    review selection sort

--- practice/selectionSort.js
+++ practice/selectionSort.js
@@ -5,5 +5,23 @@ const swap = (arr, i1, i2) => ([arr[i1], arr[i2]] = [arr[i2], arr[i1]]);
  * @param {Array} arr
  */
 function selectionSort(arr) {
-  
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (i !== minIndex) {
+      console.log(i, minIndex);
+      console.log("swap is done");
+      swap(arr, minIndex, i);
+    }
+  }
+  return arr;
 }
+
+let a = [0, 2, 100, 3, 99, 200, 4, 5];
+let b = [1, 2, 3];
+let d = [3, 3, 1] ;
+console.log(selectionSort(d));
