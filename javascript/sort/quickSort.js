bba39eacf046268ac8ea8f7b97f6291d8b6b2c24
DanielCodex <danialups1997@gmail.com>
2022-10-03 22:18:04 +0200

    for sure i know about the easier one LOL

--- javascript/sort/quickSort.js
+++ javascript/sort/quickSort.js
@@ -1,18 +1,18 @@
 const swap = (arr, i1, i2) => ([arr[i1], arr[i2]] = [arr[i2], arr[i1]]);
 
 /**
- * 
- * 
- * @param {Array} arr 
- * @param {number} start 
- * @param {number} end 
+ *
+ *
+ * @param {Array} arr
+ * @param {number} start
+ * @param {number} end
  * @return index of the pivot
  */
 function pivot(arr, start = 0, end = arr.length - 1) {
   let pivot = arr[start];
   let swapIndex = start;
 
-for (let i = start + 1; i <= end; i++){
+  for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
       swapIndex++;
       swap(arr, swapIndex, i);
@@ -28,12 +28,62 @@ for (let i = start + 1; i <= end; i++){
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
     let pivotIndex = pivot(arr, left, right);
-    // left 
+    // left
     quickSort(arr, left, pivotIndex - 1);
     // right
-    quickSort(arr, pivotIndex+1, right);
+    quickSort(arr, pivotIndex + 1, right);
   }
-  return arr
-
+  return arr;
 }
-console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+// console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+
+/**
+ *
+ * @param {Array} arr
+ */
+function simpleQuickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < arr.length; i++) {
+      pivot > arr[i] ? left.push(arr[i]) : right.push(arr[i]);
+    }
+
+    return [...simpleQuickSort(left), pivot, ...simpleQuickSort(right)];
+  }
+}
+
+let a = [4, 8, 2, 1, 5, 7, 6, 3];
+// console.log(simpleMergeSort(a));
+
+/**
+ *
+ * @param {Array} arr
+ */
+function quicksortES6(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(Math.random() * arr.length)];
+  let left = [];
+  let right = [];
+  let equal = [];
+  for (let val of arr) {
+    if (val < pivot) {
+      left.push(val);
+    } else if (val > pivot) {
+      right.push(val);
+    } else {
+      equal.push(val);
+    }
+  }
+
+  return [...quicksortES6(left), ...equal, ...quicksortES6(right)];
+}
+
+let b = [3, 2, 9, 5, 1];
+console.log(quicksortES6(b));
